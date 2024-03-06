83f7000c75eaed2a679029464bd831d2f6b201c0
adamd <adam.d.hinton@gmail.com>
2024-03-06 11:13:54 -0500

    Player class

--- src/dev/lpa/Main.java
+++ src/dev/lpa/Main.java
@@ -1,5 +1,6 @@
 package dev.lpa;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
@@ -16,13 +17,84 @@ interface ISaveable{
 
 
 
-class Player{
+class Player implements ISaveable{
     private String name;
-    private String weapon;
+    private String weapon = "sword";
     private int hitPoints;
     private int strength;
 
+    public Player(String name, int hitPoints, int strength) {
+        this.name = name;
+        this.weapon = "sword";
+        this.hitPoints = hitPoints;
+        this.strength = strength;
+    }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getWeapon() {
+        return weapon;
+    }
+
+    public void setWeapon(String weapon) {
+        this.weapon = weapon;
+    }
+
+    public int getHitPoints() {
+        return hitPoints;
+    }
+
+    public void setHitPoints(int hitPoints) {
+        this.hitPoints = hitPoints;
+    }
+
+    public int getStrength() {
+        return strength;
+    }
+
+    public void setStrength(int strength) {
+        this.strength = strength;
+    }
+
+    @Override
+    public List<String> write() {
+        List<String> solution = new ArrayList<String>();
+
+        solution.add(name);
+        solution.add("" + hitPoints);
+        solution.add("" + strength);
+        solution.add(weapon);
+
+        return solution;
+    }
+
+    @Override
+    public void read(List<String> myList) {
+        if (myList == null){
+            return;
+        }
+
+        if(myList.size() <=0){
+            return;
+        }
+
+        name = myList.get(0);
+        hitPoints = Integer.parseInt(myList.get(1));
+        strength = Integer.parseInt(myList.get(2));
+        weapon = myList.get(3);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Monster{name='%s', hitPoints=%d, strength=%d}", name, hitPoints, strength);
+
+    }
 }
 
 
