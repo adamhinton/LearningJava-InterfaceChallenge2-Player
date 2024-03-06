c95a156153f5ae8f8dc9bf23db8d27ae7e2c2d8f
adamd <adam.d.hinton@gmail.com>
2024-03-06 10:09:44 -0500

    ISaveable interface

--- src/dev/lpa/Main.java
+++ src/dev/lpa/Main.java
@@ -1,15 +1,32 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+package dev.lpa;
+
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
     }
+}
+
+interface ISaveable{
+  List<String> write();
+  void read(List<String> myList);
+}
+
+
+
+
+class Player{
+    private String name;
+    private String weapon;
+    private int hitPoints;
+    private int strength;
+
+
+}
+
+
+
+class Monster{
+
 }
